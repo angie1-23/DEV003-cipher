88338b6bbbdfdd1294c9f5b699d677e6c1a39e0d
Angie Muñoz <aemunozpl@gmail.com>
2022-11-29 22:52:18 -0500

    Funcionalidad

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,29 @@
+// const cipher = {function (mensaje){
+//   var nuevapalabra= "";
+//   for(var i=0;i<str.length;i++){
+//     if(str.charCodeAt(i)>=65&&str.charCodeAt(i)<=90){
+//       nuevapalabra+=String.fromCharCode((str.charCodeAt(i)+13-65)%26+65);
+//     }else{
+//       nuevapalabra+=String.fromCharCode(str.charCodeAt(i));
+//     }
+//   }
+//   return nuevapalabra;
+// }
+// }
+
 const cipher = {
-  // ...
-};
+  encode:function (){
+      var mensaje = document.getElementById("Texto del usuario").value;
+      var nuevapalabra= "";
+      for(var i=0;i<mensaje.length;i++){
+        if(mensaje.charCodeAt(i)>=65&&mensaje.charCodeAt(i)<=90){
+          nuevapalabra+=String.fromCharCode((mensaje.charCodeAt(i)+13-65)%26+65);
+        }else{
+          nuevapalabra+=String.fromCharCode(mensaje.charCodeAt(i));
+        }
+      }
+      alert(nuevapalabra);
+    }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import cipher from './cipher.js';
 
+document.getElementById("Accion").addEventListener("click",cipher.encode);
+
 console.log(cipher);
