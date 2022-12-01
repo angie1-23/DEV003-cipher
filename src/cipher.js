6859f1f4b7b0746ac7becd537d79cb21958b2ada
Angie Muñoz <aemunozpl@gmail.com>
2022-12-01 13:18:59 -0500

    Diseño

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,3 @@
-// const cipher = {function (mensaje){
-//   var nuevapalabra= "";
-//   for(var i=0;i<str.length;i++){
-//     if(str.charCodeAt(i)>=65&&str.charCodeAt(i)<=90){
-//       nuevapalabra+=String.fromCharCode((str.charCodeAt(i)+13-65)%26+65);
-//     }else{
-//       nuevapalabra+=String.fromCharCode(str.charCodeAt(i));
-//     }
-//   }
-//   return nuevapalabra;
-// }
-// }
-
 const cipher = {
   encode:function (){
       var mensaje = document.getElementById("Texto del usuario").value;
@@ -24,6 +11,19 @@ const cipher = {
       }
       alert(nuevapalabra);
     }
-}
+  /*decode:function (){
+      var mensaje = document.getElementById("Texto del usuario").value;
+      var offset = document.getElementById("Offset").value;
+      var nuevapalabra= "";
+      for(var i=0;i<mensaje.length;i++){
+        if(mensaje.charCodeAt(i)>=65&&mensaje.charCodeAt(i)<=90){
+          nuevapalabra+=String.fromCharCode((mensaje.charCodeAt(i)+(26-offset%26)-65)%26+65);
+        }else{
+          nuevapalabra+=String.fromCharCode(mensaje.charCodeAt(i));
+        }
+      }
+      alert(nuevapalabra);
+    }*/
+};
 
 export default cipher;
