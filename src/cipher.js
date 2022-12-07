6d1df8e22bc9002e967d4fde033e7faa05523dbd
Angie Muñoz <aemunozpl@gmail.com>
2022-12-06 21:50:21 -0500

    Cambios Totales

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,46 @@
-const cipher = {
-  encode:function (){
-      var mensaje = document.getElementById("Texto del usuario").value;
-      var nuevapalabra= "";
-      for(var i=0;i<mensaje.length;i++){
-        if(mensaje.charCodeAt(i)>=65&&mensaje.charCodeAt(i)<=90){
-          nuevapalabra+=String.fromCharCode((mensaje.charCodeAt(i)+13-65)%26+65);
-        }else{
-          nuevapalabra+=String.fromCharCode(mensaje.charCodeAt(i));
-        }
-      }
-      alert(nuevapalabra);
+/*Se crea la variable const cipher donde contiene la funcion parfa realizar el cifrado */
+const cipher = {  
+  encode:function (offset,mensaje){
+
+    if (typeof offset !== 'number' || typeof mensaje !== 'string'){
+      throw TypeError();
     }
-  /*decode:function (){
-      var mensaje = document.getElementById("Texto del usuario").value;
-      var offset = document.getElementById("Offset").value;
-      var nuevapalabra= "";
-      for(var i=0;i<mensaje.length;i++){
+    else{
+      let nuevapalabra= "";
+    
+      for(let i=0;i<mensaje.length;i++){
         if(mensaje.charCodeAt(i)>=65&&mensaje.charCodeAt(i)<=90){
-          nuevapalabra+=String.fromCharCode((mensaje.charCodeAt(i)+(26-offset%26)-65)%26+65);
-        }else{
+          nuevapalabra+=String.fromCharCode((mensaje.charCodeAt(i)+offset-65)%26+65);
+        }
+        else if(mensaje.charCodeAt(i)>=97&&mensaje.charCodeAt(i)<=122){
+          nuevapalabra+=String.fromCharCode((mensaje.charCodeAt(i)+offset-97)%26+97);
+        }
+        else{
           nuevapalabra+=String.fromCharCode(mensaje.charCodeAt(i));
         }
       }
-      alert(nuevapalabra);
-    }*/
-};
+      return nuevapalabra;
+    }
+
+  },
+
+  decode:function (offset,mensaje){
+
+    if (typeof offset === 'number' && typeof mensaje === 'string'){
+      let nuevapalabra= "";
+
+      for(let i=0;i<mensaje.length;i++){
+        if(mensaje.charCodeAt(i)>=65&&mensaje.charCodeAt(i)<=90){
+          nuevapalabra+=String.fromCharCode((mensaje.charCodeAt(i)-offset-90)%26+90);
+        }else if(mensaje.charCodeAt(i)>=97&&mensaje.charCodeAt(i)<=122){
+          nuevapalabra+=String.fromCharCode((mensaje.charCodeAt(i)+(26-offset%26)-97)%26+97);
+        }
+      }
+      return nuevapalabra; 
+    }else { 
+      throw TypeError();
+    } 
+  }
+}
 
 export default cipher;
